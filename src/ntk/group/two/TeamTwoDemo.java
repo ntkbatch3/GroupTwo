a2dce097442b1733ffbb326a87a503adc2b30daf
kakash <kakash@10.0.0.164>
2021-03-23 20:29:56 -0500

    follow the steps

--- src/ntk/group/two/TeamTwoDemo.java
+++ src/ntk/group/two/TeamTwoDemo.java
@@ -1,15 +1,23 @@
 package ntk.group.two;
 
 public class TeamTwoDemo {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Team Two ");
 		System.out.println("Eliott");
 		System.out.println("ABDO-RAZAK");
 	}
- public void Beezak() {
-	 System.out.println("first commit..");
- }
+
+	public void Beezak() {
+		System.out.println("first commit..");
+	}
+	public void steps() {
+		System.out.println("1-->stash");
+		System.out.println("2--->pull");
+		System.out.println("2--->bring your stash");
+		System.out.println("2--->commit");
+	}
+
  
  
 	public static void todo() {
