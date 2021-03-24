c0eda025175e2e522aaf4e1ee6d5b8fcba14f266
kakash <kakash@10.0.0.164>
2021-03-23 20:11:29 -0500

    abdou

--- src/ntk/group/two/TeamTwoDemo.java
+++ src/ntk/group/two/TeamTwoDemo.java
@@ -5,6 +5,7 @@ public class TeamTwoDemo {
 	public static void main(String[] args) {
 		System.out.println("Team Two ");
 		System.out.println("Eliott");
+		System.out.println("ABDO-RAZAK");
 	}
  public void Beezak() {
 	 System.out.println("first commit..");
