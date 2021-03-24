7fba9e8d58725d852ea17608cda9bf9d894df92d
emoral11 <emoral11@RADNBDSKGJ8MOM2.win.ad.jhu.edu>
2021-03-24 17:22:44 -0400

    adding 5 nums

--- src/ntk/group/two/TeamTwoDemo.java
+++ src/ntk/group/two/TeamTwoDemo.java
@@ -7,10 +7,13 @@ public class TeamTwoDemo {
 	 */
 
 	public static void main(String[] args) {
+		int sumFive;
 		System.out.println("Team Two ");
 		System.out.println("Eliott");
 		System.out.println("ABDO-RAZAK");
 		System.out.println("Hela");
+		sumFive= addingFiveNumber(1,1,1,1,1);
+		System.out.println(sumFive);
 	}
 
  public void fabio() {
@@ -20,7 +23,9 @@ public class TeamTwoDemo {
 	public static void todo() {
 		System.out.println("Follow the process");
 	}
-	public static void Day2Eliott() {
-		System.out.println("This is Day 2 test");
+	public static int addingFiveNumber(int a,int b,int c,int d,int e) {
+			int sum=a+b+c+d+e;
+			return sum ;
 	}
+	
 }
