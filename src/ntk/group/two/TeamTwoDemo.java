a90d18af810f14f1ead67ed1c53b786ba16a8800
kakash <kakash@10.0.0.164>
2021-03-23 20:58:57 -0500

    make it clean

--- src/ntk/group/two/TeamTwoDemo.java
+++ src/ntk/group/two/TeamTwoDemo.java
@@ -1,32 +1,21 @@
 package ntk.group.two;
 
 public class TeamTwoDemo {
+	
+	/*
+	 * make it clean
+	 */
 
 	public static void main(String[] args) {
 		System.out.println("Team Two ");
 		System.out.println("Eliott");
 		System.out.println("ABDO-RAZAK");
-//>>>>>>> branch 'master' of https://github.com/ntkbatch3/GroupTwo.git
 		System.out.println("Hela");
 	}
 
-	public void Beezak() {
-		System.out.println("first commit..");
-	}
-	public void steps() {
-		System.out.println("1-->stash");
-		System.out.println("2--->pull");
-		System.out.println("2--->bring your stash");
-		System.out.println("2--->commit");
-	}
-
- 
  public void fabio() {
 	 System.out.println("hope this works ");
  }
- 
- 
- 
  
 	public static void todo() {
 		System.out.println("Follow the process");
