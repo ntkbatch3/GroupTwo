9f5b7c0514a236b03f99e64f311742e6e981a598
lopez <lopez@LAPTOP-H4LQEVT9>
2021-03-24 19:32:13 -0400

    added subtract method 3/24/2021

--- src/ntk/group/two/TeamTwoDemo.java
+++ src/ntk/group/two/TeamTwoDemo.java
@@ -12,20 +12,31 @@ public class TeamTwoDemo {
 		System.out.println("Eliott");
 		System.out.println("ABDO-RAZAK");
 		System.out.println("Hela");
-		sumFive= addingFiveNumber(1,1,1,1,1);
+		sumFive = addingFiveNumber(1, 1, 1, 1, 1);
 		System.out.println(sumFive);
 	}
 
- public void fabio() {
-	 System.out.println("hope this works ");
- }
- 
+	public void fabio() {
+		System.out.println("hope this works ");
+	}
+
 	public static void todo() {
 		System.out.println("Follow the process");
 	}
-	public static int addingFiveNumber(int a,int b,int c,int d,int e) {
-			int sum=a+b+c+d+e;
-			return sum ;
+
+	public static int addingFiveNumber(int a, int b, int c, int d, int e) {
+		int sum = a + b + c + d + e;
+		return sum;
+	}
+
+	public static int subtractFiveNumber(int a, int b, int c, int d, int e) {
+		int sum = a - b - c - d - e;
+		return sum;
 	}
 	
+	
+	
+	
+	
+
 }
